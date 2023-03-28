786b242b8fb4b69a768356a21e6c03eb46d5fe35
luludmi <luana.navarro001@mymdc.net>
2023-03-28 17:17:33 -0400

    Initial Commit

---
+++ Practice app/Practice_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Practice_appApp.swift
+//  Practice app
+//
+//  Created by Luana Navarro on 3/28/23.
+//
+
+import SwiftUI
+
+@main
+struct Practice_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
